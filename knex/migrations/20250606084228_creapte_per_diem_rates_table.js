875f9c6934ba9d1f27ca8912a8222295d215b790
Pannathon Srinuntakul <pannathon.s@cipher.co.th>
2025-06-06 16:46:45 +0700

    add table accommodation rate, per diem rate

---
+++ knex/migrations/20250606084228_creapte_per_diem_rates_table.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('per_diem_rates', function(table) {
+      table.increments('id').primary();
+      table.string('position_title').nullable(); 
+      table.string('level_code').nullable(); 
+      table.enu('area_type', ['ในเขต', 'นอกเขต']).notNullable(); 
+      table.decimal('per_diem_standard', 10, 2).notNullable(); 
+      table.boolean('is_editable_per_diem').notNullable().defaultTo(false); 
+      table.decimal('max_per_diem', 10, 2).nullable(); 
+      table.decimal('meal_deduction_per_meal', 10, 2).notNullable(); 
+      table.boolean('is_editable_days').notNullable().defaultTo(true); 
+      table.timestamps(true, true);
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('per_diem_rates');
+  };
+  
