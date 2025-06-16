61438f80a4977ca890ac97fa65ecd9d539c134a5
wincipher <chawin.i@cipher.co.th>
2025-06-16 19:42:45 +0700

    update field in table approval_trip_entries, approval_work_locations

---
+++ knex/migrations/20250616123817_destination_id_destination_table_to_approval_work_locations.js.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('approval_work_locations', function(table) {
+    table.integer('destination_id').nullable();
+    table.string('destination_table').nullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('approval_work_locations', function(table) {
+    table.dropColumn('destination_id');
+    table.dropColumn('destination_table');
+  });
+};
