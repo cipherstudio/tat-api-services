18b4429a80bd907135cddc1c10e17e41830fc103
Pannathon Srinuntakul <pannathon.s@cipher.co.th>
2025-06-16 21:27:45 +0700

    logic office international, domestic

--- src/modules/master-data/dto/create-office-international.dto.ts
+++ src/modules/master-data/dto/create-office-international.dto.ts
@@ -1,4 +1,4 @@
-import { IsString } from 'class-validator';
+import { IsString, IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 /**
@@ -40,4 +40,28 @@ export class CreateOfficeInternationalDto {
   @ApiProperty({ description: 'The region of the office international' })
   @IsString()
   region: string;
+
+  /**
+   * The country ID of the office international
+   * @TypeProperty({
+   *   type: 'number',
+   *   isOptional: false,
+   *   description: 'The country ID of the office international'
+   * })
+   */
+  @ApiProperty({ description: 'The country ID of the office international' })
+  @IsNumber()
+  countryId: number;
+
+  /**
+   * The currency ID of the office international
+   * @TypeProperty({
+   *   type: 'number',
+   *   isOptional: false,
+   *   description: 'The currency ID of the office international'
+   * })
+   */
+  @ApiProperty({ description: 'The currency ID of the office international' })
+  @IsNumber()
+  currencyId: number;
 }
--- src/modules/master-data/entities/office-domestic.entity.ts
+++ src/modules/master-data/entities/office-domestic.entity.ts
@@ -1,34 +1,53 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Provinces } from './provinces.entity.js';
 
-export interface OfficeDomestic {
+export class OfficeDomestic {
   /**
    * The unique identifier for the office domestic
    */
+  @ApiProperty({ description: 'The unique identifier for the office domestic' })
   id: number;
 
   /**
    * The name of the office domestic
    */
+  @ApiProperty({ description: 'The name of the office domestic' })
   name: string;
 
   /**
    * The region of the office domestic
    */
+  @ApiProperty({ description: 'The region of the office domestic' })
   region: string;
 
   /**
    * Whether this is a head office
    */
+  @ApiProperty({ description: 'Whether this is a head office' })
   isHeadOffice: boolean;
+
+  /**
+   * The province ID of the office domestic
+   */
+  @ApiProperty({ description: 'The province ID of the office domestic' })
+  provinceId: number;
+
+  /**
+   * The province information
+   */
+  @ApiProperty({ description: 'The province information', type: () => Provinces })
+  province?: Provinces;
 
   /**
    * When the office domestic was created
    */
+  @ApiProperty({ description: 'When the office domestic was created' })
   createdAt: Date;
 
   /**
    * When the office domestic was last updated
    */
+  @ApiProperty({ description: 'When the office domestic was last updated' })
   updatedAt: Date;
 }
 
@@ -38,6 +57,7 @@ export const officeDomesticColumnMap = {
   name: 'name',
   region: 'region',
   is_head_office: 'isHeadOffice',
+  province_id: 'provinceId',
   created_at: 'createdAt',
   updated_at: 'updatedAt',
 };
@@ -48,6 +68,7 @@ export const officeDomesticReverseColumnMap = {
   name: 'name',
   region: 'region',
   isHeadOffice: 'is_head_office',
+  provinceId: 'province_id',
   createdAt: 'created_at',
   updatedAt: 'updated_at',
 }; 
