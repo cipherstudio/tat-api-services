599cabaa6964c8f9af2cc19cc8a67e8b3701bded
wincipher <chawin.i@cipher.co.th>
2025-07-02 14:36:19 +0700

    added field attachment_id (approval_budgets, approval_clothing_expense)

---
+++ knex/migrations/20250702073037_alter_approval_budgets.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('approval_budgets', (table) => {
+    table.integer('attachment_id').nullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('approval_budgets', (table) => {
+    table.dropColumn('attachment_id');
+  });
+};
