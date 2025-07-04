5d725ca4f199eb9e03781c29572392ed59548a09
wincipher <chawin.i@cipher.co.th>
2025-07-04 16:15:17 +0700

    migration files - approval_continuous + status

---
+++ knex/constants/approval_continuous_status.js
@@ -0,0 +1,16 @@
+const approvalContinuousStatus = [
+    {
+        status_code: 'PENDING',
+        label: 'รออนุมัติ'
+    },
+    {
+        status_code: 'APPROVED',
+        label: 'อนุมัติ'
+    },
+    {
+        status_code: 'REJECTED',
+        label: 'ปฏิเสธ'
+    }
+];
+
+module.exports = approvalContinuousStatus;
---
+++ knex/migrations/20250704090221_approval_continuous_status.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('approval_continuous_status', function(table) {
+    table.increments('id').primary();
+    table.string('status_code').notNullable();
+    table.string('label').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('approval_continuous_status');
+};
