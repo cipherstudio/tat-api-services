2c75135a0b17b4cbd7cc63e83a01b0ae0042f494
wincipher <chawin.i@cipher.co.th>
2025-08-16 23:36:37 +0700

    Refactor meet_rate seed to check for existing data before insertion, preventing duplicate entries.

--- knex/seeds/23_meet_rate.js
+++ knex/seeds/23_meet_rate.js
@@ -4,35 +4,38 @@
  */
 
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('meet_rate').del();
-  
-  // Inserts seed entries
-  await knex('meet_rate').insert([
-    {
-      type: 'การจัดประชุมคณะทำงานที่คณะกรรมการ ททท.แต่งตั้ง',
-      food: 35000.00,
-      snack: 15000.00
-    },
-    {
-      type: 'การจัดประชุมของคณะทำงานที่ผู้ว่าแต่งตั้ง',
-      food: 3000.00,
-      snack: 500.00
-    },
-    {
-      type: 'การจัดกระชุมที่มี นายก รองนายก หรือประธานกรรมการ ททท.เป็นประธาน',
-      food: 3500.00,
-      snack: 700.00
-    },
-    {
-      type: 'การจัดประชุมที่มีผู้ว่าการหรือพนักงานระดับ 10 ลงมาถึงหัวหน้าเป็นประธาน',
-      food: 3000.00,
-      snack: 500.00
-    },
-    {
-      type: 'การจัดประชุมเพื่อบริหารงานหรือกิจการของ ททท.',
-      food: 2000.00,
-      snack: 300.00
-    }
-  ]);
+  // Check if data already exists
+  const existingData = await knex('meet_rate');
+
+  // insert data if no data exists
+  if (existingData.length === 0) {
+    // Inserts seed entries
+    await knex('meet_rate').insert([
+      {
+        type: 'การจัดประชุมคณะทำงานที่คณะกรรมการ ททท.แต่งตั้ง',
+        food: 35000.00,
+        snack: 15000.00
+      },
+      {
+        type: 'การจัดประชุมของคณะทำงานที่ผู้ว่าแต่งตั้ง',
+        food: 3000.00,
+        snack: 500.00
+      },
+      {
+        type: 'การจัดกระชุมที่มี นายก รองนายก หรือประธานกรรมการ ททท.เป็นประธาน',
+        food: 3500.00,
+        snack: 700.00
+      },
+      {
+        type: 'การจัดประชุมที่มีผู้ว่าการหรือพนักงานระดับ 10 ลงมาถึงหัวหน้าเป็นประธาน',
+        food: 3000.00,
+        snack: 500.00
+      },
+      {
+        type: 'การจัดประชุมเพื่อบริหารงานหรือกิจการของ ททท.',
+        food: 2000.00,
+        snack: 300.00
+      }
+    ]);
+  }
 };
